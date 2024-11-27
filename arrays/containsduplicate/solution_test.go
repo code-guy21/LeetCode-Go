46c1d3ae013e403e93ff40a495abc35337e56184
Alexis San Javier <alexissj16@knights.ucf.edu>
2024-11-27 10:38:02 -0500

    Update solution_test.go

--- arrays/containsduplicate/solution_test.go
+++ arrays/containsduplicate/solution_test.go
@@ -7,7 +7,7 @@ func TestContainsDuplicate(t *testing.T){
 		nums []int
 		want bool
 	}{
-		{[]int{1, 2, 3, 1}, true},
+	{[]int{1, 2, 3, 1}, true},
         {[]int{1, 2, 3, 4}, false},
         {[]int{1, 1, 1, 3, 3, 4, 3, 2, 4, 2}, true},
         {[]int{}, false},
